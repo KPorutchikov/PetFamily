b99a2e45e84bbb71c9ee938e04fb556e5c207cc5
Konstantin <porutchikov@gmail.com>
2025-04-10 17:11:41 +0300

    Minio

---
+++ Backend/src/PetFamily.Application/Providers/IFileProvider.cs
@@ -0,0 +1,14 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Application.FileProvider;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Application.Providers;
+
+public interface IFileProvider
+{
+    Task<Result<string, Error>> UploadFile(FileData file, CancellationToken cancellationToken = default);
+    
+    Task<Result<string, Error>> RemoveFile(string objectName, CancellationToken cancellationToken = default);
+    
+    Task<Result<string, Error>> GetFileByObjectName(string objectName, CancellationToken cancellationToken = default);
+}
---
+++ Backend/src/PetFamily.Application/Volunteers/UpdateFile/GetFileHandler.cs
@@ -0,0 +1,20 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Application.Providers;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Application.Volunteers.UpdateFile;
+
+public class GetFileHandler
+{
+    private readonly IFileProvider _fileProvider;
+
+    public GetFileHandler(IFileProvider fileProvider)
+    {
+        _fileProvider = fileProvider;
+    }
+    
+    public async Task<Result<string, Error>> Handle(string objectName, CancellationToken ct = default)
+    {
+        return await _fileProvider.GetFileByObjectName(objectName, ct);
+    }
+}
